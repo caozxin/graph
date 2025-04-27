15713bfd8dbb963e124e84fc51c9eaecb3fc9111
Zhixin Cao <111973939+caozxin@users.noreply.github.com>
2025-04-27 15:27:58 -0400

    Update 1197. Minimum Knight Moves.py

--- 1197. Minimum Knight Moves.py
+++ 1197. Minimum Knight Moves.py
@@ -16,15 +16,38 @@ class Solution:
                 move_row = row + delta_row[i]
                 move_col = col + delta_col[i]
                 res.append((move_row, move_col))
-            print(res, len(res))
+            # print(res, len(res))
             return res
+        # min_count = []
+        # min_count[0] = float('inf')
+        all_path = []
+        # visited = set([starting_node])
+        def dfs(starting_node, target_node, visited, path):
+            if abs(starting_node[0]) * abs(starting_node[1]) > abs(target_node[0]) * abs(target_node[1]):
+                return
 
-        def dfs(starting_node, visited):
-
+            if starting_node == target_node:
+                print("found it!")
+                #update min_count here:
+                # if min_count > len(path[:]):
+                #     min_count = len(path[:])
+                return
+            # exit()
             for each_move in knight_moves(starting_node):
+                # exit()
                 if each_move in visited:
                     continue
+                # print("each_move", each_move)
                 visited.add(each_move)
-                dfs(each_move, visited)
+                path.append(each_move)
+                # print(visited)
+                # exit()
+                dfs(each_move,target_node, visited, path)
+                path.pop()
+                # exit()
 
         # knight_moves((0,0))
+        target_node = (x, y)
+        starting_node = (0,0)
+        # knight_moves(starting_node)
+        dfs(starting_node, target_node, set(), [])
