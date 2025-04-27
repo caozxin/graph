f4714f9022573afeb23a6b70136c5e5c957c46bf
Zhixin Cao <111973939+caozxin@users.noreply.github.com>
2025-04-27 15:49:31 -0400

    Update 1197. Minimum Knight Moves.py

--- 1197. Minimum Knight Moves.py
+++ 1197. Minimum Knight Moves.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 class Solution:
     def minKnightMoves(self, x: int, y: int) -> int:
 
@@ -6,9 +8,6 @@ class Solution:
 
         def knight_moves(coord):
             row, col = coord
-
-            # delta_row = [-1, 0, 1, 0]
-            # delta_col = [0, 1, 0, -1]
             delta_row = [-2, -2, -1, -1, 1, 1, 2, 2]
             delta_col = [-1, 1, -2, 2, -2, 2, -1, 1]
             res = []
@@ -16,38 +15,27 @@ class Solution:
                 move_row = row + delta_row[i]
                 move_col = col + delta_col[i]
                 res.append((move_row, move_col))
-            # print(res, len(res))
+
             return res
-        # min_count = []
-        # min_count[0] = float('inf')
-        all_path = []
-        # visited = set([starting_node])
-        def dfs(starting_node, target_node, visited, path):
-            if abs(starting_node[0]) * abs(starting_node[1]) > abs(target_node[0]) * abs(target_node[1]):
-                return
 
-            if starting_node == target_node:
-                print("found it!")
-                #update min_count here:
-                # if min_count > len(path[:]):
-                #     min_count = len(path[:])
-                return
-            # exit()
-            for each_move in knight_moves(starting_node):
-                # exit()
-                if each_move in visited:
-                    continue
-                # print("each_move", each_move)
-                visited.add(each_move)
-                path.append(each_move)
-                # print(visited)
-                # exit()
-                dfs(each_move,target_node, visited, path)
-                path.pop()
-                # exit()
+        def bfs(starting_node):
+            queue = deque([starting_node])
+            visited = set([starting_node])
+            steps = 0
+            while len(queue) > 0:
+                
+                for _ in range(len(queue)):
+                    node = queue.popleft()
+                    # print("node", node)
+                    if node == (x, y):
+                        return steps
+                    for each_move in knight_moves(node):
+                        if each_move in visited:
+                            continue
 
-        # knight_moves((0,0))
-        target_node = (x, y)
-        starting_node = (0,0)
-        # knight_moves(starting_node)
-        dfs(starting_node, target_node, set(), [])
+                        queue.append(each_move)
+                        visited.add(each_move)
+
+                steps += 1 # steps here is similar to level.
+
+        return bfs((0,0))
