b5c62ad8dad008793248a1bf21706b74b709a3b8
Zhixin Cao <111973939+caozxin@users.noreply.github.com>
2025-05-04 07:09:49 -0400

    Update 773. Sliding Puzzle.py

--- 773. Sliding Puzzle.py
+++ 773. Sliding Puzzle.py
@@ -18,22 +18,47 @@ class Solution:
                 neighbor_row = row + delta_row[i]
                 neighbor_col = col + delta_col[i]
                 if 0 <= neighbor_row < num_rows and 0 <= neighbor_col < num_cols:
-                    if board[neighbor_row][neighbor_col] == 0: # it needs to be empty space. 
-                        res.append((neighbor_row, neighbor_col)) 
+                    # if board[neighbor_row][neighbor_col] == 0: # it needs to be empty space. 
+                    res.append((neighbor_row, neighbor_col)) 
             print(res)
             return res
-        
-        def bfs(starting_node, target):
-            queue = deque([(starting_node, 1, board_state)])
+        # def new_board_state(node, board_state):
+        #     for 
+        def bfs(starting_node):
+            queue = deque([(starting_node, 0, board)])
             visited = set([starting_node])
 
             while len(queue) > 0:
 
                 for _ in range(len(queue)): # here just to track the level during traversal in bfs. 
                     node, level, board_state = queue.popleft()
-                    new_board_state = board_state
-                    if new_board_state == goal_board:
+                    print("node, level, board_state," , node, level, board_state)
+                    # new_board_state = board_state
+                    if board_state == goal_board:
                         return level
-
-                    for neighbor in get_neighbors(node):
+                    # print("node, get_neighbors(node)", node, get_neighbors(node))
+                    # exit()
+                    for neighbor in get_neighbors(node): # the neighbor_node is always empty. 
+                        if neighbor in visited:
+                            continue
+                        #here we update the board_state :
+                        board_state[node[0]][node[1]], board_state[neighbor[0]][neighbor[1]] = board_state[neighbor[0]][neighbor[1]], 0
+                        print(board_state[node[0]][node[1]], board_state[neighbor[0]][neighbor[1]],board_state )
                         
+                        
+                        # board_state[node[0]][node[1]], board_state[neighbor[0]][neighbor[1]] = board_state[neighbor[0]][neighbor[1]], 0
+                        # if board_state[neighbor[0]][neighbor[1]] == 0:
+                        #     board_state[neighbor[0]][neighbor[1]] = board_state[node[0]][node[1]]
+                        #     board_state[node[0]][node[1]] = 0
+                        queue.append((neighbor, level + 1, board_state))
+                        
+                        visited.add(neighbor)
+                    exit()
+
+            return -1 # if no path
+
+        # we should start with empty space
+        for i in range(num_rows):
+            for j in range(num_cols):
+                if board[i][j] == 0:
+                    bfs((i,j))
