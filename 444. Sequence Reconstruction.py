33e6c111645ba7c3ebc7513ca38e734c29baaa4b
Zhixin Cao <111973939+caozxin@users.noreply.github.com>
2025-05-07 07:03:08 -0400

    Update 444. Sequence Reconstruction.py

--- 444. Sequence Reconstruction.py
+++ 444. Sequence Reconstruction.py
@@ -11,7 +11,24 @@ class Solution:
                     indegree[neighbor] += 1
             return indegree
 
-        # topo_sort
+        # topo_sort --> TODO
+        def topo_sort(graph) -> List[List[int]]:
+            res = []
+            queue = deque()
+            indegree = find_indegree(graph)
+            print(indegree)
+            for node in indegree:
+                queue.append(node)
+
+            while len(queue) > 0:
+                node = queue.popleft()
+                res.append(node)
+                for neighbor in graph[node]:
+                    indegree[node] -= 1
+                    if indegree[neighbor] == 0:
+                        queue.append(neighbor)
+            print(res)
+            # return res if len(graph) == len(res) else None
 
         # contruct the graph: for each_num in nums, it should be dict[each_num] = [list of its outgoing edges]
         graph: dict[str, list[str]] = {t: [] for t in nums}
@@ -20,6 +37,7 @@ class Solution:
             for i in range(len(each)):
                 graph[each[i]] =  graph[each[i]]  + each[i+1:]
                 
-        print(graph, find_indegree(graph))
+        # print(graph, find_indegree(graph))
+        topo_sort(graph)
 
         
