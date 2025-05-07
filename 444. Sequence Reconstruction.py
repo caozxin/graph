59d83545f267bc461c1d53a5c347b93cfd338647
Zhixin Cao <111973939+caozxin@users.noreply.github.com>
2025-05-07 06:41:20 -0400

    Update 444. Sequence Reconstruction.py

--- 444. Sequence Reconstruction.py
+++ 444. Sequence Reconstruction.py
@@ -4,7 +4,13 @@ class Solution:
     def sequenceReconstruction(self, nums: List[int], sequences: List[List[int]]) -> bool:
 
         # find_indegree of all nodes
-        
+        def find_indegree(graph):
+            indegree = {node: 0 for node in graph}
+            for node in graph:
+                for neighbor in graph[node]:
+                    indegree[neighbor] += 1
+            return indegree
+
         # topo_sort
 
         # contruct the graph: for each_num in nums, it should be dict[each_num] = [list of its outgoing edges]
@@ -12,7 +18,8 @@ class Solution:
         # print(graph)
         for each in sequences:
             for i in range(len(each)):
-                print(each[i], each[i+1:])
                 graph[each[i]] =  graph[each[i]]  + each[i+1:]
                 
-        print(graph)
+        print(graph, find_indegree(graph))
+
+        
