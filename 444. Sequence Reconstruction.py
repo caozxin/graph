6a9a30ad7d3e320629eec2df87dc0c8da70f6818
Zhixin Cao <111973939+caozxin@users.noreply.github.com>
2025-05-07 06:08:42 -0400

    Update 444. Sequence Reconstruction.py

--- 444. Sequence Reconstruction.py
+++ 444. Sequence Reconstruction.py
@@ -1,1 +1,18 @@
+from collections import deque
 
+class Solution:
+    def sequenceReconstruction(self, nums: List[int], sequences: List[List[int]]) -> bool:
+
+        # find_indegree of all nodes
+        
+        # topo_sort
+
+        # contruct the graph: for each_num in nums, it should be dict[each_num] = [list of its outgoing edges]
+        graph: dict[str, list[str]] = {t: [] for t in nums}
+        # print(graph)
+        for each in sequences:
+            for i in range(len(each)):
+                print(each[i], each[i+1:])
+                graph[each[i]] =  graph[each[i]]  + each[i+1:]
+                
+        print(graph)
