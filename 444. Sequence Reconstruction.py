e36b7a9147188fd71d539341e99038e27e0dc82d
Zhixin Cao <111973939+caozxin@users.noreply.github.com>
2025-05-08 06:49:26 -0400

    Update 444. Sequence Reconstruction.py

--- 444. Sequence Reconstruction.py
+++ 444. Sequence Reconstruction.py
@@ -1,3 +1,5 @@
+### my not working version:
+
 from collections import deque
 
 class Solution:
@@ -49,4 +51,62 @@ class Solution:
         # print(graph, find_indegree(graph))
         return topo_sort(graph)
 
+# improved working version:
+from collections import deque
+from typing import List
+class Solution:
+    def sequenceReconstruction(self, nums: List[int], sequences: List[List[int]]) -> bool:
+
+        # find_indegree of all nodes
+        def find_indegree(graph):
+            indegree = {node: 0 for node in graph}
+            for node in graph:
+                for neighbor in graph[node]:
+                    indegree[neighbor] += 1
+            return indegree
+
+        # topo_sort --> TODO
+        def topo_sort(graph) -> List[int]:
+            res = []
+            queue = deque()
+            indegree = find_indegree(graph)
+
+            for node in indegree:
+                if indegree[node] == 0:
+                    queue.append(node)
+
+            while len(queue) > 0:
+                if len(queue) > 1:
+                    return False
+
+                node = queue.popleft()
+                res.append(node)
+
+                # print('graph[node]', graph[node])
+                for neighbor in graph[node]:
+                    indegree[neighbor] -= 1
+                    if indegree[neighbor] == 0:
+                        queue.append(neighbor)
+            return res == nums
+
+        # contruct the graph: for each_num in nums, it should be dict[each_num] = [list of its outgoing edges]
+        # graph: dict[str, list[str]] = {t: [] for t in nums}
+        # # print(graph)
+        # for each in sequences:
+        #     for i in range(len(each)):
+        #         graph[each[i]] =  graph[each[i]]  + each[i+1:]
+                
+                # construct the graph
+        graph = {num: [] for num in nums}
+        for seq in sequences:
+            for i in range(len(seq) - 1):
+                if seq[i+1] not in graph[seq[i]]:
+                    graph[seq[i]].append(seq[i+1])
+            for num in seq:
+                if num not in graph:
+                    graph[num] = []
+
+        return topo_sort(graph)
+
         
+
